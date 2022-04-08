330df9de826eb9fe8f2d5f8e7342641f0cff32f8
Amir Hosein Javadi <javadiamirhosein.2000@gmail.com>
2022-04-08 18:13:59 +0430

    Add files via upload

--- CreateDataset_NoDelay.m
+++ CreateDataset_NoDelay.m
@@ -1,8 +1,10 @@
 clc;
 clear all;
 close all;
-Train_iter = 1e5; % number of dataset columns
-fd = 10;
+Fc = 6e9; % 10 Gig
+C = 3e8;
+fd = 1000;
+v = (fd*C/Fc)*3.6;
 frame_size = 16;
 L = 4;
 P = 4;
@@ -11,16 +13,19 @@ t = (0:1/bitrate: (frame_size+L+P-1)/bitrate).';
 alpha_arr = [0.9,0.85,0.8,0.75,0.7];
 Tap_Number = 4;
 SNR_max = 15;
+%%
+Train_iter = 1e5; % number of dataset columns
 data_input_train = zeros(Train_iter*size(alpha_arr,2), 4*2 + 1); % 4 pilot and alpha in SEFDM
 data_output_train = zeros(Train_iter*size(alpha_arr,2), 4 + 4); % amplitude and doopler freq
 for j = 1:size(alpha_arr,2)
     alpha = alpha_arr(j);
-    F = SEFDM_Modulation(alpha,size(t,1));
+    F = SEFDM_Modulation(alpha,size(t,1)); % ...... |||| ...... %
     for i =1:Train_iter
         SNR = rand()*SNR_max+15;
         Delay = zeros(1,4);
         Doppler_Frequency = rand(4,1)*fd;
-        Amplitude = raylrnd(1,4,1);
+        % rician
+        Amplitude = raylrnd(1,4,1);  % parameter ---> tecnical report
         data_output_train((j-1)*Train_iter+i,:) = [Doppler_Frequency;Amplitude];
         S = (randi([0,1],frame_size,1)*2-1) + 1i*(randi([0,1],frame_size,1)*2-1);
         Sx = [S(end-3:end);S(1:6);ones(4,1);S(7:16)];  % 4 cp, 6 signal, 4 pilot, 10 signal
@@ -37,8 +42,8 @@ output_text = ["Doppler_Freq_1","Doppler_Freq_2","Doppler_Freq_3","Doppler_Freq_
               "Amplitude_1","Amplitude_2","Amplitude_3","Amplitude_4"];   
 csvwrite('data_output_train.txt',data_output_train)
 csvwrite('data_input_train.txt',data_input_train)
-
-Validation_iter = 1e3;
+%%
+Validation_iter = 1e4;
 data_input_validation = zeros(Validation_iter*size(alpha_arr,2), 4*2 + 1);
 data_output_validation = zeros(Validation_iter*size(alpha_arr,2), 4 + 4);
 for j = 1:size(alpha_arr,2)
@@ -49,19 +54,18 @@ for j = 1:size(alpha_arr,2)
         Delay = zeros(1,4);
         Doppler_Frequency = rand(4,1)*fd;
         Amplitude = raylrnd(1,4,1);
-        data_output_validation((j-1)*Train_iter+i,:) = [Doppler_Frequency;Amplitude];
+        data_output_validation((j-1)*Validation_iter+i,:) = [Doppler_Frequency;Amplitude];
         S = (randi([0,1],frame_size,1)*2-1) + 1i*(randi([0,1],frame_size,1)*2-1);
         Sx = [S(end-3:end);S(1:6);ones(4,1);S(7:16)];  % 4 cp, 6 signal, 4 pilot, 10 signal
         S_TX = conj(F) * Sx;
         S_RX = channel(t,S_TX,Tap_Number,Amplitude,Doppler_Frequency,Delay);
         Noisy_Signal = awgn(S_RX,SNR,'measured');
-        data_input_validation((j-1)*Train_iter+i,:) = [real(Noisy_Signal(11:14));imag(Noisy_Signal(11:14));alpha];
+        data_input_validation((j-1)*Validation_iter+i,:) = [real(Noisy_Signal(11:14));imag(Noisy_Signal(11:14));alpha];
     end
 end
 csvwrite('data_output_validation.txt',data_output_validation)
 csvwrite('data_input_validation.txt',data_input_validation)
-
-
+%%
 function F = SEFDM_Modulation(alpha,n)
     F = zeros(n,n);
     for i = 0:n-1
